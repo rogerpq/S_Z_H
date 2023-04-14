2f01e0d5a700ee9f5f6d2b55367ec384118e2153
Hussein <118964436+SS7SS@users.noreply.github.com>
2023-04-14 15:10:23 +0300

    Update create.py

--- Zaid/modules/basic/create.py
+++ Zaid/modules/basic/create.py
@@ -4,29 +4,29 @@ from pyrogram.types import Message
 from Zaid.modules.help import add_command_help
 
 
-@Client.on_message(filters.command("create", ".") & filters.me)
+@Client.on_message(filters.command("انشاء", ".") & filters.me)
 async def create(client: Client, message: Message):
     if len(message.command) < 3:
         return await message.edit_text(
-            message, f"**Type .help create if you need help**"
+            message, f"**اكتب .help create إذا كنت بحاجة إلى مساعدة**"
         )
     group_type = message.command[1]
     split = message.command[2:]
     group_name = " ".join(split)
-    xd = await message.edit_text("`Processing...`")
-    desc = "Welcome To My " + ("Group" if group_type == "gc" else "Channel")
-    if group_type == "gc":  # for supergroup
+    xd = await message.edit_text("`جار...`")
+    desc = "Welcome To My " + ("كروب" if group_type == "كروب" else "قناة")
+    if group_type == "كروب":  # for supergroup
         _id = await client.create_supergroup(group_name, desc)
         link = await client.get_chat(_id["id"])
         await xd.edit(
-            f"**Successfully Created Telegram Group: [{group_name}]({link['invite_link']})**",
+            f"**تم انشاء المجموعة بنجاح : [{group_name}]({link['invite_link']})**",
             disable_web_page_preview=True,
         )
-    elif group_type == "ch":  # for channel
+    elif group_type == "قناة":  # for channel
         _id = await client.create_channel(group_name, desc)
         link = await client.get_chat(_id["id"])
         await xd.edit(
-            f"**Successfully Created Telegram Channel: [{group_name}]({link['invite_link']})**",
+            f"**تم انشاء القناة ينجاح : [{group_name}]({link['invite_link']})**",
             disable_web_page_preview=True,
         )
 
