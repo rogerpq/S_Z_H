42a631c6f921cdc73d47f7ef00467b6d5b3ff9d8
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-04-15 18:03:23 +0300

    تعريب محمد  (حمد) - @PPF22

--- Zaid/modules/basic/البروفايل.py
+++ Zaid/modules/basic/البروفايل.py
@@ -83,79 +83,79 @@ async def extract_user(message):
     return (await extract_user_and_reason(message))[0]
 
 @Client.on_message(
-    filters.command(["unblock"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["الغاء البلوك"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def unblock_user_func(client: Client, message: Message):
     user_id = await extract_user(message)
-    tex = await message.reply_text("`Processing . . .`")
+    tex = await message.reply_text("`انتظر قليلًا جاي الغاء البلوك عنه . . .`")
     if not user_id:
         return await message.edit(
-            "Provide User ID/Username or reply to user message to unblock."
+            "يجب عليك الرد على المستخدم او الغاء البلوك + يوزره أو ايديه."
         )
     if user_id == client.me.id:
-        return await tex.edit("Ohk done ✅.")
+        return await tex.edit("تم إلغاء البلوك عنه.")
     await client.unblock_user(user_id)
     umention = (await client.get_users(user_id)).mention
-    await message.edit(f"**Successfully Unblocked** {umention}")
+    await message.edit(f"**تم إلغاء الحظر عن** {umention}")
 
 @Client.on_message(
-    filters.command(["block"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["بلوك"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def block_user_func(client: Client, message: Message):
     user_id = await extract_user(message)
-    tex = await message.reply_text("`Processing . . .`")
+    tex = await message.reply_text("`جاري حظره . . .`")
     if not user_id:
         return await tex.edit_text(
             "Provide User ID/Username or reply to user message to block."
         )
     if user_id == client.me.id:
-        return await tex.edit_text("ohk ✅.")
+        return await tex.edit_text("تم حظره ✅.")
     await client.block_user(user_id)
     umention = (await client.get_users(user_id)).mention
     await tex.edit_text(f"**Successfully blocked** {umention}")
 
 
 @Client.on_message(
-    filters.command(["setname"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["وضع اسم"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def setname(client: Client, message: Message):
-    tex = await message.reply_text("`Processing . . .`")
+    tex = await message.reply_text("`جاري وضع اسم . . .`")
     if len(message.command) == 1:
         return await tex.edit(
-            "Provide a text to set as your name."
+            "عذرًا عزيزي عليك كتابة الأسم أولًا"
         )
     elif len(message.command) > 1:
         name = message.text.split(None, 1)[1]
         try:
             await client.update_profile(first_name=name)
-            await tex.edit(f"**Successfully Changed Your Name To** `{name}`")
+            await tex.edit(f"**تم تغيير الأسم الخاص بك إلى** `{name}`")
         except Exception as e:
-            await tex.edit(f"**ERROR:** `{e}`")
+            await tex.edit(f"**خطأ:** `{e}`")
     else:
         return await tex.edit(
-            "Provide a text to set as your name."
+            "أدخل نصًا لتعيينه كاسمك."
         )
 
 @Client.on_message(
-    filters.command(["setbio"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["وضع بايو"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def set_bio(client: Client, message: Message):
-    tex = await message.edit_text("`Processing . . .`")
+    tex = await message.edit_text("`جاري وضع بايو . . .`")
     if len(message.command) == 1:
         return await tex.edit("Provide text to set as bio.")
     elif len(message.command) > 1:
         bio = message.text.split(None, 1)[1]
         try:
             await client.update_profile(bio=bio)
-            await tex.edit(f"**Successfully Change your BIO to** `{bio}`")
+            await tex.edit(f"**تم تغيير البايو الخاص بك إلى.** `{bio}`")
         except Exception as e:
-            await tex.edit(f"**ERROR:** `{e}`")
+            await tex.edit(f"**خطأ:** `{e}`")
     else:
-        return await tex.edit("Provide text to set as bio.")
+        return await tex.edit("يجب عليك كتابة النص لتعيينه البايو.")
 
 
 @Client.on_message(
-    filters.command(["setpfp"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["وضع صورة"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def set_pfp(client: Client, message: Message):
     replied = message.reply_to_message
@@ -171,17 +171,17 @@ async def set_pfp(client: Client, message: Message):
         await client.set_profile_photo(profile_photo)
         if os.path.exists(profile_photo):
             os.remove(profile_photo)
-        await message.reply_text("**Your Profile Photo Changed Successfully.**")
+        await message.reply_text("**تم تغيير الصورة الخاص بك.**")
     else:
         await message.reply_text(
-            "Reply to any photo to set as profile photo"
+            "يجب عليك الرد على الصورة لتعيينها لحسابك"
         )
         await sleep(3)
         await message.delete()
 
 
 @Client.on_message(
-    filters.command(["vpfp"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["وضع فيديو"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def view_pfp(client: Client, message: Message):
     user_id = await extract_user(message)
@@ -190,7 +190,7 @@ async def view_pfp(client: Client, message: Message):
     else:
         user = await client.get_me()
     if not user.photo:
-        await message.reply_text("Profile photo not found!")
+        await message.reply_text("لم يتم العثور على صورة الملف الشخصي!")
         return
     await client.download_media(user.photo.big_file_id, file_name=profile_photo)
     await client.send_photo(
