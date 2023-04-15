51ac21aede7dcf0d206b2f311fe738dd65ac2518
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-04-15 03:44:18 +0300

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @Rickthon_Support × @Rickthon - @S_Z_H - @PPF22 •",
+                f"```{str(ac)}```\n• @rickthon_group × @RICKTHON - @S_Z_H - @PPF22 •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **الأمر:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Rickthon - @PPF22 - @S_Z_H"
+            this_command += "© @RICKTHON - @PPF22 - @S_Z_H"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Rickthon_Support × @Rickthon •"
+            message, f"```{str(ac)}```\n• @Rickthon_group × @Rickthon •"
         )
         await message.reply(
             f"**إستعمال**:`.مساعدة البث` **لعرض تفاصيل الوحدة**"
