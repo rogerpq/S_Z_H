bd1154dd1e758e6f2b59b598b980a3c7596d6561
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-04-19 22:50:58 +0300

    سيف قم بإضافة الايدي مالتي

--- Zaid/modules/basic/invite.py
+++ Zaid/modules/basic/invite.py
@@ -9,32 +9,32 @@ from Zaid.modules.help import add_command_help
 
 
 @Client.on_message(
-    filters.command(["invite"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["ضيف"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def inviteee(client: Client, message: Message):
     mg = await message.reply_text("`Adding Users!`")
     user_s_to_add = message.text.split(" ", 1)[1]
     if not user_s_to_add:
-        await mg.edit("`Give Me Users To Add! Check Help Menu For More Info!`")
+        await mg.edit("`أعطني مستخدمين لإضافتهم! تحقق من قائمة التعليمات لمزيد من المعلومات!`")
         return
     user_list = user_s_to_add.split(" ")
     try:
         await client.add_chat_members(message.chat.id, user_list, forward_limit=100)
     except BaseException as e:
-        await mg.edit(f"`Unable To Add Users! \nTraceBack : {e}`")
+        await mg.edit(f"`غير قادر على إضافة المستخدمين! \nTraceBack : {e}`")
         return
-    await mg.edit(f"`Sucessfully Added {len(user_list)} To This Group / Channel!`")
+    await mg.edit(f"`تم الإضافة {len(user_list)} إلى هذه المجموعة / القناة!`")
 
 @Client.on_message(
-    filters.command(["inviteall"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["دعوة الكل"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def inv(client: Client, message: Message):
-    ex = await message.reply_text("`Processing . . .`")
+    ex = await message.reply_text("`جاري المعالجة . . .`")
     text = message.text.split(" ", 1)
     queryy = text[1]
     chat = await client.get_chat(queryy)
     tgchat = message.chat
-    await ex.edit_text(f"inviting users from {chat.username}")
+    await ex.edit_text(f"دعوة المستخدمين من {chat.username}")
     async for member in client.get_chat_members(chat.id):
         user = member.user
         zxb = [
@@ -51,14 +51,14 @@ async def inv(client: Client, message: Message):
             except Exception as e:
                 pass
 
-@Client.on_message(filters.command("invitelink", ".") & filters.me)
+@Client.on_message(filters.command("الرابط", ".") & filters.me)
 async def invite_link(client: Client, message: Message):
-    um = await message.edit_text("`Processing...`")
+    um = await message.edit_text("`جاري المعالجة...`")
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
         message.chat.title
         try:
             link = await client.export_chat_invite_link(message.chat.id)
-            await um.edit(f"**Link Invite:** {link}")
+            await um.edit(f"**رابط الدعوة:** {link}")
         except Exception:
             await um.edit("Denied permission")
 
@@ -67,10 +67,10 @@ add_command_help(
     "invite",
     [
         [
-            "invitelink",
-            "Get your private invite link. [Need Admin]",
+            "رابط الدعوة",
+            "إعطاء رابط الدعوة الخاص بالمجموعة. [تحتاج مشرف]",
         ],
-        ["invite @username", "to invite someone."],
-        ["inviteall @username", "Mass adding (can affect your account)."],
+        ["دعوة @username", "لإضافة بعض الأشخاص."],
+        ["دعوة الكل @username", "الجمع الشامل (يمكن أن تؤثر على حسابك)."],
     ],
 )
