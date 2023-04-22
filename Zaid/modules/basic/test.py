9d2dfa2638c71ba77412279f55dd2fb603bb9da5
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-04-22 16:25:41 +0300

    Update test.py

--- Zaid/modules/basic/test.py
+++ Zaid/modules/basic/test.py
@@ -5,8 +5,7 @@ from Zaid.modules.help import add_command_help
 
 
 @Client.on_message(filters.command('تفسير','.') & filters.me)
-async def chat_broadcast(c: Client, m: Message,online=False)
-async def say(_, message: Message):
+async def say(client, message: Message):
     if len(message.command) == 1:
         return
     command = " ".join(message.command[1:])
