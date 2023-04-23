30cd1b7d4f87f29598be2602757a6ab1089bb000
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-04-23 14:29:04 +0300

    Update aal.py

--- Zaid/modules/bot/aal.py
+++ Zaid/modules/bot/aal.py
@@ -7,7 +7,7 @@ from pyrogram import Client
  
 
 ALIVE_PIC = 'https://graph.org//file/6ef37b30de52ad0a6f8ee.mp4'
-@Client.on_message(filters.command(["awake", "alive"], [".", "!"]) & filters.me)
+@app.on_message(filters.command(["awake", "alive"], [".", "!"]))
 async def allive(client: Client, e: Message):
     try:
         me = await client.get_me()
