33a2bba6287a56a3067ca7e6fb6c4794456ae45c
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-04-23 23:34:44 +0300

    Update apmguarde.py

--- Zaid/modules/private/apmguarde.py
+++ Zaid/modules/private/apmguarde.py
@@ -9,38 +9,38 @@ import Zaid.database.pmpermitdb as Zaid
 
 
 
-@Client.on_message(filters.command("pmguard", ["."]) & filters.me)
+@Client.on_message(filters.command("حماية الخاص", ["."]) & filters.me)
 async def pmguard(client, message):
     arg = get_arg(message)
     if not arg:
-        await message.edit("**I only understand on or off**")
+        await message.edit("**انا يمكنني فهم فقط التفعيل او الايقاف!**")
         return
-    if arg == "off":
+    if arg == "ايقاف":
         await Zaid.set_pm(False)
-        await message.edit("**PM Guard Deactivated**")
-    if arg == "on":
+        await message.edit("**تم ايقاف حماية الخاص بنجاح**")
+    if arg == "تفعيل":
         await Zaid.set_pm(True)
-        await message.edit("**PM Guard Activated**")
-@Client.on_message(filters.command("setpmmsg", ["."]) & filters.me)
+        await message.edit("**تم تشغيل حماية الخاص بنجاح**")
+@Client.on_message(filters.command("ضع رسالة التحذير", ["."]) & filters.me)
 async def setpmmsg(client, message):
     arg = get_arg(message)
     if not arg:
-        await message.edit("**What message to set**")
+        await message.edit("**ماهي الرساله التي يجب تعيينها؟**")
         return
-    if arg == "default":
+    if arg == "الرسالة العادية":
         await Zaid.set_permit_message(Zaid.PMPERMIT_MESSAGE)
-        await message.edit("**Anti_PM message set to default**.")
+        await message.edit("**تم ارجاع رساله التحذير العادية**.")
         return
     await Zaid.set_permit_message(f"`{arg}`")
-    await message.edit("**Custom anti-pm message set**")
+    await message.edit("**تم وضع رساله التحذير الخاصه بك بنجاح!**")
 
 
 add_command_help(
-    "antipm",
+    "حماية الخاص",
     [
-        [".pmguard [on or off]", " -> Activates or deactivates anti-pm."],
-        [".setpmmsg [message or default]", " -> Sets a custom anti-pm message."],
-        [".setblockmsg [message or default]", "-> Sets custom block message."],
+        [".حماية الخاص [تفعيل او ايقاف]", " -> لتشغيل حماية الخاص او اطفاء حماية الخاص."],
+        [".ضع رسالة التحذير [الرسالة او الرسالة العادية]", " -> لوضع رسالة ترحيب خاصة بك."],
+        [".ضع رسالة الحظر [message or default]", "-> Sets custom block message."],
         [".setlimit [value]", " -> This one sets a max. message limit for unwanted PMs and when they go beyond it, bamm!."],
         [".allow", " -> Allows a user to PM you."],
         [".deny", " -> Denies a user to PM you."],
