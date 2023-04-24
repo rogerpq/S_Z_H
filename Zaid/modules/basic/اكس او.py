95ed910c3e94212c6a6826c25d8843fd42e8b0b2
BesooGa <122231055+BesooGa@users.noreply.github.com>
2023-04-24 22:27:40 +0300

    Create اكس او.py

---
+++ Zaid/modules/basic/اكس او.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+from Zaid.database import cli as database
+from Zaid.helper.PyroHelpers import ReplyCheck
+#Dev file: @V_IRUuS
+#Edit file : @newbaghdad
+#دراكون_كان_هنا
+@Client.on_message(filters.command("اكس او", ".") & filters.me)
+async def clone(client: Client, message: Message):
+	result = await app.get_inline_bot_results("xoBot", "play")
+	await msg.delete()
+	await app.send_inline_bot_result(msg.chat.id, result.query_id, result.results[0].id)
+	
+	add_command_help(
+"اكس او",
+    [
+[".اكس او", " بهاذا الامر ستلعب لعبه الاكس او الشهيره"],
+    ],
+)
