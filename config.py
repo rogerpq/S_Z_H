c43e85df81a2dfc4ef5b8cffb744454eae83f8a0
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-06-28 23:33:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
+REDIS_URL = getenv("REDIS_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph//file/6ef37b30de52ad0a6f8ee.mp4')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
@@ -21,13 +21,4 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/rick1128/S_Z_H")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
-STRING_SESSION6 = getenv("STRING_SESSION6", "")
-STRING_SESSION7 = getenv("STRING_SESSION7", "")
-STRING_SESSION8 = getenv("STRING_SESSION8", "")
-STRING_SESSION9 = getenv("STRING_SESSION9", "")
-STRING_SESSION10 = getenv("STRING_SESSION10", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
