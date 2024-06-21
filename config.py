49cca11dc700c08dbcd94f9cef1bdf07d2951ecf
روجر <104784913+rogerpq@users.noreply.github.com>
2024-06-21 16:52:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
-REDIS_URL = getenv("REDIS_URL")
+REDIS_URL = os.environ.get("SCALINGO_REDIS_URL", None)
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph//file/6ef37b30de52ad0a6f8ee.mp4')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/rick1128/S_Z_H")
+REPO_URL = getenv("REPO_URL", "https://github.com/rogerpq/S_Z_H")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION = getenv("STRING_SESSION", "")
